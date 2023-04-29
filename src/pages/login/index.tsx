50e1ec61b8122ba7d62e0c9276ebad0f992b4aa3
Ocloa <lexasen231@mail.ru>
2023-04-29 18:39:46 +0700

    Initial commit

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,12 @@
+import next from "next/types";
+import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
+import LoginForm from '../../components/login';
+
+export default function Login() {
+    return (
+        <LoginForm/>
+
+    )
+  }
